40bdd77bc7fe55fd16aa61893db40f4218afd710
Ki-zu-na <www.3438538167@gmail.com>
2025-02-13 06:51:04 +0800

    misc

--- data_processing/json_adapter_dataset.py
+++ data_processing/json_adapter_dataset.py
@@ -68,8 +68,8 @@ def get_prompt_embeddings_chunked(prompt: str, tokenizer: CLIPTokenizer, text_en
 
     concatenated_prompt_embeds = torch.cat(prompt_embeds_list, dim=1) if prompt_embeds_list else None # 拼接 prompt_embeds
     # pooled_prompt_embeds 可以选择最后一个 chunk 的，或者平均，这里选择平均
-    pooled_prompt_embeds = torch.stack(pooled_prompt_embeds_list, dim=0) if pooled_prompt_embeds_list else None
-    pooled_prompt_embeds = pooled_prompt_embeds.mean(dim=0)
+    pooled_prompt_embeds = concatenated_prompt_embeds.mean(dim=1, keepdim=True)
+    pooled_prompt_embeds = pooled_prompt_embeds.squeeze(0)
 
     return concatenated_prompt_embeds, pooled_prompt_embeds
 class JSONAdapterDataset(Dataset):
